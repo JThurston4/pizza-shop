3f57c103ebfa04258bf4f4e2fcc6768eee934134
Joshua Thurston <joshthurston92@gmail.com>
2018-11-08 15:11:01 -0700

    second commit

--- app/components/pizza-controller.js
+++ app/components/pizza-controller.js
@@ -1,10 +1,22 @@
 import PizzaService from "./pizza-service.js";
 
 
-let pizzaService = new PizzaService
+let _pizzaService = new PizzaService
 
 export default class PizzaController {
   constructor() {
     console.log("Hello from the controller")
+  }
+
+  pizzaBuilder(event) {
+    event.preventDefault()
+    console.log(event)
+    let form = event.target
+    let formData = {
+      name: form.name.value
+    }
+    _pizzaService.pizzaBuilder(formData)
+
+    form.reset()
   }
 }
--- app/components/pizza-service.js
+++ app/components/pizza-service.js
@@ -1,8 +1,17 @@
+import Pizza from "../models/pizza-model.js";
+import Toppings from "../models/toppings-model.js";
 
-
+let toppings = []
+// let _pizza = new Pizza
 
 export default class PizzaService {
   constructor() {
     console.log("services checking in")
+  }
+
+  pizzaBuilder(formData) {
+    let topping = new Toppings(formData)
+    toppings.push(topping)
+    console.log(toppings)
   }
 }
